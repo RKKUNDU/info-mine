ccf8c926c8439467b8e0467b224dc1dfa9f66533
RKKUNDU <kundu.rohit06@gmail.com>
2020-11-06 22:46:07 +0530

    Change Key class: key is in bytes, store key as string in file

--- security/keys.py
+++ security/keys.py
@@ -1,11 +1,15 @@
 from cryptography.fernet import Fernet 
 
 class Key:
+    '''
+    self.key is in bytes
+    store key in the file as string
+    '''
     def __init__(self):
         '''
         Initializes the encryption key
         '''
-        self.key = Fernet.generate_key().decode('utf-8')
+        self.key = Fernet.generate_key()
     
     def read_key_from_file(self, path):
         '''
@@ -14,7 +18,7 @@ class Key:
             path (string): path to the file
         '''
         f = open(path, "r")
-        self.key = f.read()
+        self.key = bytes(f.read(), 'utf-8')
 
     def write_key_to_file(self, path):
         '''
@@ -23,7 +27,7 @@ class Key:
             path (string): path to the file
         '''
         f = open(path, "w")
-        f.write(self.key)
+        f.write(self.key.decode('utf-8'))
 
     def encrypt(self, txt):
         '''
@@ -46,7 +50,7 @@ class Key:
             decrypted text (string)
         '''
         f = Fernet(self.key)
-        txt = f.decrypt(encrypted)
+        txt = f.decrypt(bytes(encrypted, 'utf-8')).decode('utf-8')
         return txt
 
     def encrypt_and_write_to_file(self, txt, path):
@@ -65,9 +69,11 @@ class Key:
         Read from a file and decrypt the text
         Input:
             path (string): path of the file to be read from
+        Returns:
+            decrypted file (string)
         '''
-        f = open(path, "rb")
+        f = open(path, "r")
         txt = f.read()
-        decrypted = self.decrypt(txt).decode('utf-8')
-        print(decrypted)
+        decrypted = self.decrypt(txt)
+        return decrypted
         
