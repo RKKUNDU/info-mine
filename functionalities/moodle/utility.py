6c08746d92710eec42ed2d54f44d43182be84fa1
RKKUNDU <kundu.rohit06@gmail.com>
2020-11-18 04:21:04 +0530

    Update moodle credential to handle exceptions

--- functionalities/moodle/utility.py
+++ functionalities/moodle/utility.py
@@ -7,11 +7,19 @@ class utils:
     def get_credential():
         '''
         fetch moodle credential from config file or USER INPUT
+        Prompt user to give user correct credentials until correct credentials given or
+        the user gives 'q' or 'quit' as id
         '''
         moodle = moodle_credential()
         if moodle.get_stored_token() is None or moodle.get_stored_userid is None:
-            id, password = utils.take_input()
-            moodle.write_credential(id, password)   
+            credential_set = False
+            while not credential_set:
+                print("Invalid username/password. Type q or quit to exit")
+                id, password = utils.take_input()
+                if id == 'q' or id == 'quit':
+                    exit(0)
+
+                credential_set = moodle.write_credential(id, password)
 
         return moodle.get_stored_token(), moodle.get_stored_userid()
 
@@ -19,7 +27,10 @@ class utils:
         '''
         Take moodle credential from user
         '''
-        id = input("Enter your Moodle User ID: ")
+        id = input("Enter your Moodle User ID: ")   
+        if id == 'q' or id == 'quit':
+            return id, None
+
         password = getpass.getpass()
         return id, password
 
--- security/moodle_credentials.py
+++ security/moodle_credentials.py
@@ -2,19 +2,30 @@ from security.keys import Key
 import requests
 from bs4 import BeautifulSoup as bs
 import json
+import os
+
 PATH = 'configs/' # set the path wrt to the calling function
 class moodle_credential:
     def __init__(self):
+        '''
+        Read token, userid from file if exists
+        '''
+        self.token = None
+        self.userid = None
         self.key = Key()
         try:
             self.key.read_key_from_file(PATH + "moodle.key")
         except Exception as ex:
-            print("Error: ", ex)
-            #TODO: Handle exceptions
-
-        self.token = None
-        self.userid = None
+            # print("Error: ", ex)
+            if os.path.exists(PATH + 'moodle.key'):
+                os.remove(PATH + 'moodle.key')
+            
+            return
+        
         if self.key.key == "":
+            if os.path.exists(PATH + 'moodle.key'):
+                os.remove(PATH + 'moodle.key')
+            
             return
 
         try:    
@@ -30,31 +41,55 @@ class moodle_credential:
                     self.token = value
             
         except Exception as err:
-            print("Error: ", err)
+            if os.path.exists(PATH + 'moodle.key'):
+                os.remove(PATH + 'moodle.key')
+
+            # print("Error: ", err)
 
     def get_stored_token(self):
+        '''
+        return stored token
+        '''
         return self.token
 
     def get_stored_userid(self):
+        '''
+        return store userid
+        '''
         return self.userid
 
     def write_credential(self, id, password):
+        '''
+        Wrtite the (token, userid) to the file configs/moodle
+        Returns:
+            True: when valid credentials given
+            False: otherwise
+        '''
+        self.key = Key()
         self.key.write_key_to_file(PATH + 'moodle.key')
         self.token = self.get_token(id, password)
 
         # invalid login credentials or some other issue
         if self.token is None:
-            return None
+            return False
 
         self.userid = self.get_userid(self.token)
         
         if self.userid is None:
-            return None
+            return False
 
         txt = f"token={self.token},userid={self.userid}"
-        self.key.encrypt_and_write_to_file(txt, PATH + 'moodle')
+        try:
+            self.key.encrypt_and_write_to_file(txt, PATH + 'moodle')
+        except Exception as er:
+            return False
+
+        return True
 
     def get_token(self, id, password):
+        '''
+        find the token 
+        '''
         payload = {'username': id,'password': password,'service': 'moodle_mobile_app'}
         try:
             res = requests.post('https://moodle.iitb.ac.in/login/token.php', payload)
@@ -64,19 +99,31 @@ class moodle_credential:
             else:
                 return None
         except Exception as err:
-            print("Error: ", err)
+            # print("Error: ", err)
+            if os.path.exists(PATH + 'moodle.key'):
+                os.remove(PATH + 'moodle.key')
+
             return None
 
     def get_userid(self, token):
+        '''
+        find the userid
+        '''
         payload = {'wstoken': token,'wsfunction': 'core_webservice_get_site_info'} 
         try:
             res = requests.post('https://moodle.iitb.ac.in/webservice/rest/server.php?moodlewsrestformat=json',payload)
             res = json.loads(res.content)
             if "exception" in res.keys():
-                print("Error: ", res['errorcode'])
+                # print("Error: ", res['errorcode'])
+                if os.path.exists(PATH + 'moodle.key'):
+                    os.remove(PATH + 'moodle.key')
+
                 return None
             else:
                 return res['userid']
         except Exception as err:
-            print("Error: ", err)
+            # print("Error: ", err)
+            if os.path.exists(PATH + 'moodle.key'):
+                os.remove(PATH + 'moodle.key')
+
             return None
