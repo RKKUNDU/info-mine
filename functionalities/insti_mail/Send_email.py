d56966135e97c2b5f0ebd2882bbdbbeab87fdfbc
pranshu1202 <57661561+pranshu1202@users.noreply.github.com>
2020-11-17 13:57:51 +0530

    Update Send_email.py

--- functionalities/insti_mail/Send_email.py
+++ functionalities/insti_mail/Send_email.py
@@ -1,28 +1,73 @@
 
 import smtplib
 import ssl
+import os
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from email.mime.base import MIMEBase
+from email import encoders
+from insti_credentials import insti_credentials
+try:
+    Cred=insti_credentials()
+    username = str(Cred.get_stored_username())
+    password=str(Cred.get_stored_password())
 
-f = open("key.txt", "r")
-username=str(f.readlines(1))
-username=username[2:-4]
-password=str(f.readlines(2))
-password=password[2:-2]
+    host = 'smtp-auth.iitb.ac.in'
+    port = int(587)
 
-host = 'smtp-auth.iitb.ac.in'
-port = int(587)
+    sender = username
+    targets=[]
+    N=int(input('Please Enter number of people you want to send your E-mail : '))
+    for i in range(0,N):
+        temp = input('Enter E-mail : ' )
+        targets.append(temp)
 
-sender = '203050098@iitb.ac.in'
-targets = ['203050030@iitb.ac.in','pranshuchourasia786@gmail.com']
+    msg = MIMEMultipart()
+    Sub = input('Please Enter Subject : ')
+    msg['Subject'] = Sub
+    msg['From'] = sender
+    msg['To'] = ', '.join(targets)
+    # msg['CC'] = ', '.join(CC)
+    # msg['BCC'] = ', '.join(BCC)
+    Body = input('Please Enter body : ')
+    txt = MIMEText(Body)
+    msg.attach(txt)
 
-msg = MIMEText('Hi, i am pranshu ?')
-msg['Subject'] = 'Hello'
-msg['From'] = sender
-msg['To'] = ', '.join(targets)
+    choice = str(input('Do you have attachment y/n : ')).lower()
+    if choice == 'y':
+        filepath = input('Enter full path of file : ')
+    
+        type_file = filepath.split('.')[-1]
+        print(type_file)
+        if type_file == 'txt':
+            with open(filepath, "rb") as f:
+                file1 = MIMEApplication(f.read(),_subtype='octet-stream', _encoder=encoders.encode_base64,)
+                file1.add_header('Content-Disposition','attachment',filename=os.path.basename(filepath))
+                msg.attach(file1)
+
+        elif type_file == 'jpg' or type_file == 'png':
+            with open(filepath, 'rb') as f:
+                file1 = MIMEImage(f.read())
+                file1.add_header('Content-Disposition','attachment',filename=os.path.basename(filepath))
+                msg.attach(file1)
+
+        else :
+            with open(filepath, "rb") as f:
+                file1 = MIMEApplication(f.read(),_subtype=type_file)
+                file1.add_header('Content-Disposition','attachment',filename=os.path.basename(filepath))
+                msg.attach(file1)
+
+    server = smtplib.SMTP(host, port)
+    server.starttls(context=ssl.create_default_context())
+    server.login(username, password)
+    server.sendmail(sender, targets, msg.as_string())
+    print("Email sent successfully !!!")
+    server.quit()
+except:
+    print("Sending failed !! Check username / password ")
+    os.remove("configs/insti_mail")
+    os.remove("configs/insti_mail.key")
 
 
-server = smtplib.SMTP(host, port)
-server.starttls(context=ssl.create_default_context())
-server.login(username, password)
-server.sendmail(sender, targets, msg.as_string())
-server.quit()
