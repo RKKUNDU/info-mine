4cfde826fb4c5b34c8d756d7765f04331099f5ce
Rohit Kundu <36533918+RKKUNDU@users.noreply.github.com>
2020-11-18 12:43:53 +0530

    Update infomine-installer.sh

--- infomine-installer.sh
+++ infomine-installer.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# get the source code
 wget https://github.com/RKKUNDU/info-mine/archive/main.zip
 
+# unzip
 unzip main.zip
 
+# remove the zip after extraction
 rm main.zip
 
 # install python
@@ -14,16 +17,14 @@ pip3 install cryptography==2.8
 pip3 install tabulate==0.8.7
 pip3 install requests==2.22.0
 pip3 install beautifulsoup4==4.9.3
- 
 
-#echo "Source Codes are currently present in $(pwd)/info-mine-main directory. Do you want to move the source code to somewhere else? [y/n]"
-
-#read 
-
+# create infomine executable
 echo "#!/bin/bash
 cd $(pwd)/info-mine-main/
 python3 parser.py \${@}" > infomine
 
+# add permission
 chmod +x infomine
 
+# move it to bin directory
 sudo mv infomine /bin/infomine
